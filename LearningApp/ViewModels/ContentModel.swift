54e06fea6f9484b9fe3c38bd51350bef26668005
andyBarbaro <barbaro.3@osu.edu>
2022-07-20 16:52:43 -0400

    add assests, data files, and models; parse json and style data into ContentModel

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -9,5 +9,47 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    var styleData:Data?
     
+    init() {
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                
+                let modules = try decoder.decode([Module].self, from: jsonData)
+                self.modules = modules
+                
+            } catch {
+                print("Error decoding json file.")
+            }
+                
+            
+        } catch {
+            print("Error fetching data from json.")
+        }
+        
+        let styleUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+             
+            let styleData = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = styleData
+            
+        } catch {
+            print("Couldn't parse style data")
+        }
+    }
 }
